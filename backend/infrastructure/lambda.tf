a67b54a7d4c29d8b0d61b4b56d11d13f7f807d3e
ryhhill1998 <ryhhill1998@outlook.com>
2025-08-11 09:58:03 +0100

    added tf for lambda infra and basic lambda test code

---
+++ backend/infrastructure/lambda.tf
@@ -0,0 +1,11 @@
+resource "aws_lambda_function" "user_spotify_data_retrieval_lambda" {
+  function_name = "user_spotify_data_retrieval_lambda"
+  role          = aws_iam_role.user_spotify_data_retrieval_lambda.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.user_spotify_data_retrieval_lambda.repository_url}:latest"
+
+  memory_size = 512
+  timeout     = 30
+
+  architectures = ["arm64"]
+}
