f57f082831910f05d14559b386a0eb9f00a33bfa
ryhhill1998 <ryhhill1998@outlook.com>
2025-08-16 11:07:08 +0100

    added ruff check to check pr for lambda. updated lambda code to remove unused imports

--- backend/lambdas/user_spotify_data_retrieval/lambda_function.py
+++ backend/lambdas/user_spotify_data_retrieval/lambda_function.py
@@ -1,7 +1,2 @@
-import psycopg2
-print("psycopg2 successfully imported")
-
-
 def handler(event, context):
-    """Function that checks whether psycopg2  and pandas are successfully imported or not"""
-    return {"Status": "psycopg2 successfully imported"}
+    return {"Status": "Running"}
