10067d9f285caea71c678a1bcecac858b86e1972
ryhhill1998 <ryhhill1998@outlook.com>
2025-08-16 18:46:06 +0100

    added Base to db.py

--- backend/lambdas/user_spotify_data_retrieval/src/core/db.py
+++ backend/lambdas/user_spotify_data_retrieval/src/core/db.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 engine = create_engine("postgresql+psycopg2://scott:tiger@localhost/")
 
