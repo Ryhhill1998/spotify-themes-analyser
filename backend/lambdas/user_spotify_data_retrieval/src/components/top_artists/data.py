a527f51793e78c38bb2e33c4c98b3d45c6d138bd
ryhhill1998 <ryhhill1998@outlook.com>
2025-08-17 08:59:47 +0100

    finished methods for retrieving top artists

--- backend/lambdas/user_spotify_data_retrieval/src/components/top_artists/data.py
+++ backend/lambdas/user_spotify_data_retrieval/src/components/top_artists/data.py
@@ -1,3 +1,4 @@
+from src.components.top_artists.models.domain import SpotifyArtist, TopArtist
 from httpx import AsyncClient
 
 
@@ -20,8 +21,8 @@ class TopArtistsDataRetriever:
             raise Exception(f"Failed to retrieve data: {response.text}")
 
         return response.json()
-
-    async def get_top_artists(self, access_token: str, time_range: str, limit: int = 50) -> list:
+    
+    async def _get_top_artists_data(self, access_token: str, time_range: str, limit: int = 50) -> dict:
         data = await self._get_data_from_api(
             endpoint="me/top/artists", access_token=access_token, params={"time_range": time_range, "limit": limit}
         )
@@ -29,5 +30,16 @@ class TopArtistsDataRetriever:
         if not data or "items" not in data:
             raise ValueError("No top artists found or invalid response format.")
 
-        return data.get("items", [])
+        items = data.get("items", [])
+
+        return items
+
+    async def get_top_artists(self, access_token: str, time_range: str, limit: int = 50) -> list:
+        items = await self._get_top_artists_data(access_token=access_token, time_range=time_range, limit=limit)
+
+        # validate data format
+        spotify_artists = [SpotifyArtist(**item) for item in items]
+        top_artists = [TopArtist.from_spotify_artist(artist) for artist in spotify_artists]
+
+        return top_artists
         
