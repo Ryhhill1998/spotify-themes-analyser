8e0160fefb742ea0f2f14e8eb9bb16040542db88
ryhhill1998 <ryhhill1998@outlook.com>
2025-08-19 08:45:38 +0100

    renamed profile to user in data.py

---
+++ backend/lambdas/user_spotify_data_retrieval/src/components/users/__init__.py
@@ -0,0 +1,19 @@
+from src.shared.spotify.config import SpotifySettings
+from src.shared.spotify.client import spotify_client
+from src.components.top_artists.data import SpotifyTopArtistsService
+from src.components.top_artists.repository import TopArtistsRepository
+from src.components.top_artists.orchestrator import TopArtistsOrchestrator
+from backend.shared.db import SessionLocal
+
+
+def get_top_artists_orchestrator() -> TopArtistsOrchestrator:
+    db_session = SessionLocal()
+    spotify_settings = SpotifySettings()
+    data = SpotifyTopArtistsService(client=spotify_client, base_url=spotify_settings.base_url)
+    repository = TopArtistsRepository(db_session)
+    top_artists_service = TopArtistsOrchestrator(data=data, repository=repository)
+
+    return top_artists_service
+    
+
+__all__ = ["get_top_artists_orchestrator"]
--- backend/lambdas/user_spotify_data_retrieval/src/components/users/data.py
+++ backend/lambdas/user_spotify_data_retrieval/src/components/users/data.py
@@ -1,15 +1,15 @@
-from src.components.profile.models.domain import SpotifyProfile, Profile
+from src.components.users.models.domain import SpotifyUser, User
 from src.shared.spotify.data_service import SpotifyDataService
 from httpx import AsyncClient
 
 
-class SpotifyProfileService(SpotifyDataService):
+class SpotifyUserService(SpotifyDataService):
     def __init__(self, client: AsyncClient, base_url: str):
         super().__init__(client=client, base_url=base_url)
 
-    async def get_profile(self, access_token: str) -> Profile:
+    async def get_user(self, access_token: str) -> User:
         data = await self._get_data(endpoint="me", access_token=access_token)
-        spotify_profile = SpotifyProfile(**data)
-        profile = Profile.from_spotify_profile(spotify_profile)
+        spotify_user = SpotifyUser(**data)
+        user = User.from_spotify_user(spotify_user)
 
-        return profile
+        return user
