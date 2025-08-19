343afe36fb2839a39910261caee5231474a057f9
ryhhill1998 <ryhhill1998@outlook.com>
2025-08-19 08:41:13 +0100

    added orchestrator class for profile

--- backend/lambdas/user_spotify_data_retrieval/src/components/profile/orchestrator.py
+++ backend/lambdas/user_spotify_data_retrieval/src/components/profile/orchestrator.py
@@ -1,25 +1,14 @@
+from src.components.profile.models.domain import Profile
+from src.components.profile.data import SpotifyProfileService
+from src.components.profile.repository import ProfileRepository
+
+
 class ProfileOrchestrator:
-    def __init__(self, repository: ):
+    def __init__(self, data: SpotifyProfileService, repository: ProfileRepository):
+        self.data = data
         self.repository = repository
-        self.spotify_service = SpotifyTopArtistsService()
 
-    def get_top_artists(self, user_id: str, time_range: str) -> list[TopArtist]:
-        top_artists = self.spotify_service.get_top_artists(user_id=user_id, time_range=time_range)
-        collection_date = date.today()
-
-        previous_top_artists = self.repository.get_previous_top_artists(user_id=user_id, time_range=time_range)
-
-        if not previous_top_artists:
-            return self._store_and_return_top_artists(user_id, top_artists, time_range, collection_date)
-
-        return self._update_and_return_top_artists(user_id, top_artists, previous_top_artists, time_range, collection_date)
-
-    def _store_and_return_top_artists(self, user_id: str, top_artists: list[TopArtist], time_range: str, collection_date: date) -> list[TopArtist]:
-        self.repository.store_top_artists(user_id=user_id, top_artists=top_artists, time_range=time_range, collection_date=collection_date)
-        return top_artists
-
-    def _update_and_return_top_artists(self, user_id: str, top_artists: list[TopArtist], previous_top_artists: list[PreviousTopArtist], time_range: str, collection_date: date) -> list[TopArtist]:
-        for artist in top_artists:
-            artist.calculate_position_change(previous_top_items=previous_top_artists)
-
-        self._calculate_and_populate_position_change(previous_top_items=previous_top_artists, current_top_items=top_artists)    
+    async def get_and_store_user_profile(self, access_token: str) -> Profile:
+        profile = await self.data.get_profile(access_token=access_token)
+        self.repository.store_user_profile(profile=profile)
+        return profile
