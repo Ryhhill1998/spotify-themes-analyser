1fd39eaa07e9014407b1f27dc87b50d266d57ae0
ryhhill1998 <ryhhill1998@outlook.com>
2025-09-21 16:31:03 +0100

    improved top tracks pipeline exception handling

--- backend/lambdas/user_spotify_data_retrieval/src/pipelines/top_tracks_pipeline.py
+++ backend/lambdas/user_spotify_data_retrieval/src/pipelines/top_tracks_pipeline.py
@@ -3,14 +3,22 @@ from src.repositories.artists_repository import ArtistsRepository
 from src.models.domain import Artist, TopTrack, Track
 from src.utils.calculations import calculate_position_changes
 from src.models.enums import TimeRange
-from src.repositories.top_items.top_tracks_repository import TopTracksRepository
+from src.repositories.top_items.top_tracks_repository import (
+    TopTracksRepository,
+    TopTracksRepositoryException,
+)
 from src.repositories.tracks_repository import TracksRepository
-from src.services.spotify_service import SpotifyService
+from src.services.spotify_service import SpotifyService, SpotifyServiceException
+
+
+class TopTracksPipelineException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
 
 
 class TopTracksPipeline:
     def __init__(
-        self, 
+        self,
         spotify_service: SpotifyService,
         artists_repository: ArtistsRepository,
         tracks_repository: TracksRepository,
@@ -22,41 +30,64 @@ class TopTracksPipeline:
         self.top_tracks_repository = top_tracks_repository
 
     async def run(
-        self, access_token: str, user_id: str, time_range: TimeRange, collection_date: date
+        self,
+        access_token: str,
+        user_id: str,
+        time_range: TimeRange,
+        collection_date: date,
     ) -> list[Track]:
-        # 1. Get top tracks from Spotify API
-        tracks: list[Track] = await self.spotify_service.get_user_top_tracks(access_token=access_token, time_range=time_range)
-
-        # 2. Extract unique artist ids from tracks
-        unique_artist_ids: set[str] = set(artist.id for track in tracks for artist in track.artists)
-
-        # 3. Get full artist details from Spotify API
-        artists: list[Artist] = await self.spotify_service.get_artists_by_ids(access_token=access_token, artist_ids=list(unique_artist_ids))
-
-        # 4. Persist artists to DB
-        self.artists_repository.upsert_many(artists)
-
-        # 5. Persist tracks to DB
-        self.tracks_repository.upsert_many(tracks)
-
-        # 6. Create TopTracks
-        top_tracks: list[TopTrack] = [
-            TopTrack(
-                user_id=user_id,
-                track_id=track.id,
-                collection_date=collection_date,
-                time_range=time_range,
-                position=index + 1,
+        try:
+            # 1. Get top tracks from Spotify API
+            tracks: list[Track] = await self.spotify_service.get_user_top_tracks(
+                access_token=access_token, time_range=time_range
             )
-            for index, track in enumerate(tracks)
-        ]
 
-        # 7. Calculate position changes
-        previous_top_tracks: list[TopTrack] = self.top_tracks_repository.get_previous_top_items(user_id=user_id, time_range=time_range)
-        calculate_position_changes(previous_items=previous_top_tracks, current_items=top_tracks)
+            # 2. Extract unique artist ids from tracks
+            unique_artist_ids: set[str] = set(
+                artist.id for track in tracks for artist in track.artists
+            )
 
-        # 8. Store in DB
-        self.top_tracks_repository.add_many(top_tracks)
+            # 3. Get full artist details from Spotify API
+            artists: list[Artist] = await self.spotify_service.get_artists_by_ids(
+                access_token=access_token, artist_ids=list(unique_artist_ids)
+            )
 
-        # 9. Return the list of tracks
-        return tracks
+            # 4. Persist artists to DB
+            self.artists_repository.upsert_many(artists)
+
+            # 5. Persist tracks to DB
+            self.tracks_repository.upsert_many(tracks)
+
+            # 6. Create TopTracks
+            top_tracks: list[TopTrack] = [
+                TopTrack(
+                    user_id=user_id,
+                    track_id=track.id,
+                    collection_date=collection_date,
+                    time_range=time_range,
+                    position=index + 1,
+                )
+                for index, track in enumerate(tracks)
+            ]
+
+            # 7. Calculate position changes
+            previous_top_tracks: list[TopTrack] = (
+                self.top_tracks_repository.get_previous_top_items(
+                    user_id=user_id, time_range=time_range
+                )
+            )
+            calculate_position_changes(
+                previous_items=previous_top_tracks, current_items=top_tracks
+            )
+
+            # 8. Store in DB
+            self.top_tracks_repository.add_many(top_tracks)
+
+            # 9. Return the list of tracks
+            return tracks
+        except (SpotifyServiceException, TopTracksRepositoryException) as e:
+            raise TopTracksPipelineException("Top tracks pipeline failed.") from e
+        except Exception as e:
+            raise TopTracksPipelineException(
+                "Unexpected error in top tracks pipeline."
+            ) from e
