5b1469b180e55ae24e7390841e6bcc99873e58d6
ryhhill1998 <ryhhill1998@outlook.com>
2025-09-21 16:36:34 +0100

    improved top genres pipeline exception handling

--- backend/lambdas/user_spotify_data_retrieval/src/pipelines/top_genres_pipeline.py
+++ backend/lambdas/user_spotify_data_retrieval/src/pipelines/top_genres_pipeline.py
@@ -6,15 +6,21 @@ from src.repositories.top_items.top_genres_repository import TopGenresRepository
 from src.models.enums import TimeRange
 from src.models.domain import Artist, TopGenre
 
+
+class TopGenresPipelineException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
 class TopGenresPipeline:
     def __init__(self, top_genres_repository: TopGenresRepository):
         self.top_genres_repository = top_genres_repository
 
     @staticmethod
     def _get_top_genres(
-        artists: list[Artist], 
-        user_id: str, 
-        time_range: TimeRange, 
+        artists: list[Artist],
+        user_id: str,
+        time_range: TimeRange,
         collection_date: date,
     ) -> list[TopGenre]:
         # find most common genres
@@ -27,32 +33,42 @@ class TopGenresPipeline:
 
         return [
             TopGenre(
-                user_id=user_id, 
-                collection_date=collection_date, 
-                time_range=time_range, 
-                position=index + 1, 
-                genre_id=genre, 
+                user_id=user_id,
+                collection_date=collection_date,
+                time_range=time_range,
+                position=index + 1,
+                genre_id=genre,
                 percentage=round(count / total, 2),
             )
             for index, (genre, count) in enumerate(most_common_genres)
         ]
 
     def run(
-        self, 
-        artists: list[Artist], 
-        user_id: str, 
-        time_range: TimeRange, 
+        self,
+        artists: list[Artist],
+        user_id: str,
+        time_range: TimeRange,
         collection_date: date,
     ) -> None:
-        top_genres = self._get_top_genres(
-            artists=artists, 
-            user_id=user_id, 
-            time_range=time_range, 
-            collection_date=collection_date,
-        )
+        try:
+            top_genres = self._get_top_genres(
+                artists=artists,
+                user_id=user_id,
+                time_range=time_range,
+                collection_date=collection_date,
+            )
 
-        previous_top_genres: list[TopGenre] = self.top_genres_repository.get_previous_top_items(user_id=user_id, time_range=time_range)
-        calculate_position_changes(previous_items=previous_top_genres, current_items=top_genres)
+            previous_top_genres: list[TopGenre] = (
+                self.top_genres_repository.get_previous_top_items(
+                    user_id=user_id, time_range=time_range
+                )
+            )
+            calculate_position_changes(
+                previous_items=previous_top_genres, current_items=top_genres
+            )
 
-        self.top_genres_repository.add_many(top_genres)
-        
+            self.top_genres_repository.add_many(top_genres)
+        except Exception as e:
+            raise TopGenresPipelineException(
+                "Unexpected error in top genres pipeline."
+            ) from e
