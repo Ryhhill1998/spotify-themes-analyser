78b3d6d517790dd39ce418587936acf41a9eaaa8
ryhhill1998 <ryhhill1998@outlook.com>
2025-09-22 09:33:12 +0100

    simplified top emotions pipeline

--- backend/lambdas/user_spotify_data_retrieval/src/pipelines/top_emotions_pipeline.py
+++ backend/lambdas/user_spotify_data_retrieval/src/pipelines/top_emotions_pipeline.py
@@ -1,24 +1,18 @@
-import asyncio
 from collections import defaultdict
 from datetime import date
 from statistics import mean
 import heapq
 
-from backend.lambdas.user_spotify_data_retrieval.src.services.lyrics.lyrics_service import (
-    LyricsService,
+from src.services.emotional_profiles.emotional_profiles_service import (
+    EmotionalProfilesService,
 )
-from src.repositories.track_emotional_profiles_repository import (
-    TrackEmotionalProfilesRepository,
-)
+from src.services.lyrics.lyrics_service import LyricsService
 from src.repositories.top_items.top_emotions_repository import TopEmotionsRepository
-from backend.lambdas.user_spotify_data_retrieval.src.services.emotional_profiles.model_service import (
-    ModelService,
-)
 from src.utils.calculations import calculate_position_changes
 from src.models.domain import (
     TrackEmotionalProfileRequest,
-    TrackLyricsRequest,
     TrackLyrics,
+    TrackLyricsRequest,
     TopEmotion,
     Track,
     TrackEmotionalProfile,
@@ -35,34 +29,12 @@ class TopEmotionsPipeline:
     def __init__(
         self,
         lyrics_service: LyricsService,
-        emotional_profile_service: ModelService,
-        emotional_profile_repository: TrackEmotionalProfilesRepository,
+        emotional_profile_service: EmotionalProfilesService,
         top_emotions_repository: TopEmotionsRepository,
     ):
         self.lyrics_service = lyrics_service
         self.emotional_profile_service = emotional_profile_service
-        self.emotional_profile_repository = emotional_profile_repository
         self.top_emotions_repository = top_emotions_repository
-
-    async def _get_track_emotional_profile(
-        self, request: TrackEmotionalProfileRequest
-    ) -> TrackEmotionalProfile:
-        emotional_profile = await self.emotional_profile_service.get_emotional_profile(
-            request.lyrics
-        )
-        return TrackEmotionalProfile(
-            track_id=request.track_id, emotional_profile=emotional_profile
-        )
-
-    async def _get_several_track_emotional_profiles(
-        self, emotional_profile_requests: list[TrackEmotionalProfileRequest]
-    ) -> list[TrackEmotionalProfile]:
-        tasks = [
-            self._get_track_emotional_profile(request)
-            for request in emotional_profile_requests
-        ]
-        results = await asyncio.gather(*tasks, return_exceptions=True)
-        return [res for res in results if isinstance(res, TrackEmotionalProfile)]
 
     @staticmethod
     def _aggregate_emotions(
@@ -100,14 +72,14 @@ class TopEmotionsPipeline:
 
     @staticmethod
     def _get_top_emotions(
-        emotional_profile_responses: list[TrackEmotionalProfile],
+        emotional_profiles: list[TrackEmotionalProfile],
         user_id: str,
         time_range: TimeRange,
         collection_date: date,
         n: int = 5,
     ) -> list[TopEmotion]:
         average_emotion_percentages = TopEmotionsPipeline._aggregate_emotions(
-            emotional_profile_responses
+            emotional_profiles
         )
 
         top_emotions_dict = TopEmotionsPipeline._rank_and_normalise_emotions(
@@ -133,23 +105,6 @@ class TopEmotionsPipeline:
         time_range: TimeRange,
         collection_date: date,
     ) -> None:
-        track_ids = set(track.id for track in tracks)
-
-        # see which tracks already have emotional profiles stored
-        existing_track_emotional_profiles: list[TrackEmotionalProfile] = (
-            self.emotional_profile_repository.get_many(track_ids)
-        )
-        track_ids -= set(
-            profile.track_id for profile in existing_track_emotional_profiles
-        )
-
-        # see which of the remaining tracks have lyrics stored
-        existing_track_lyrics: list[TrackLyrics] = self.lyrics_repository.get_many(
-            track_ids
-        )
-        track_ids -= set(lyrics.track_id for lyrics in existing_track_lyrics)
-
-        # create lyrics request objects from tracks
         lyrics_requests = [
             TrackLyricsRequest(
                 track_id=track.id,
@@ -157,44 +112,26 @@ class TopEmotionsPipeline:
                 track_artist=track.artists[0].name,
             )
             for track in tracks
-            if track.id in track_ids
         ]
-
-        # get track lyrics from lyrics service and store in db
-        new_track_lyrics: list[TrackLyrics] = await self._get_several_track_lyrics(
+        track_lyrics: list[TrackLyrics] = self.lyrics_service.get_many_lyrics(
             lyrics_requests
         )
-        self.lyrics_repository.add_many(new_track_lyrics)
-
-        # create emotional profile requests from lyrics
-        all_track_lyrics: list[TrackLyrics] = [
-            *new_track_lyrics,
-            *existing_track_lyrics,
-        ]
 
         emotional_profile_requests = [
             TrackEmotionalProfileRequest(
                 track_id=lyrics.track_id,
                 lyrics=lyrics.lyrics,
             )
-            for lyrics in all_track_lyrics
+            for lyrics in track_lyrics
         ]
-
-        # get emotional profile responses from emotional profile service and store in db
-        new_track_emotional_profiles: list[TrackEmotionalProfile] = (
-            await self._get_several_track_emotional_profiles(emotional_profile_requests)
+        track_emotional_profiles: list[TrackEmotionalProfile] = (
+            await self.emotional_profile_service.get_many_emotional_profiles(
+                emotional_profile_requests
+            )
         )
-        print(f"{new_track_emotional_profiles = }")
-        self.emotional_profile_repository.add_many(new_track_emotional_profiles)
-
-        # calculate top emotions
-        all_track_emotional_profiles: list[TrackEmotionalProfile] = [
-            *new_track_emotional_profiles,
-            *existing_track_emotional_profiles,
-        ]
 
         top_emotions: list[TopEmotion] = self._get_top_emotions(
-            emotional_profile_responses=all_track_emotional_profiles,
+            emotional_profiles=track_emotional_profiles,
             user_id=user_id,
             time_range=time_range,
             collection_date=collection_date,
